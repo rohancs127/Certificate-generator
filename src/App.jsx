ee6863a73fbd1c4cad83ed56a0ca650d22f230f4
Rohan <rohankumar5803@gmail.com>
2023-11-26 20:54:44 +0530

    added and imported Input.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
 import Home from './assets/pages/Home';
 import Certificate from './assets/pages/Cerificate';
+import Input from './assets/pages/Input';
 
 function App() {
 
   return (
-    <Home/>
+    <Input/>
   )
 }
 
---
+++ src/assets/pages/Input.jsx
@@ -0,0 +1,11 @@
+const Input = ()=>{
+    return(
+        <section>
+            <form>
+                <input type="text" placeholder="Name"></input>
+            </form>
+        </section>
+    )
+}
+
+export default Input;

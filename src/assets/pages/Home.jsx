b5790dfd22c068a2a54e71778a2ee5765abc3f04
Rohan <rohankumar5803@gmail.com>
2023-11-28 20:33:43 +0530

    updated files and created input css file

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = ()=>{
             <div className={HomeCss.contentBody}>
                 <h3 className={HomeCss.getStarted}>Get Started</h3>
                 <h4 className={HomeCss.generateCertificate}>Generate your certificate here!</h4>
-                <button className={HomeCss.generateButton}>Generate</button>
+                <button className={HomeCss.generateButton}>Generate Certificate</button>
             </div>
             <div className={HomeCss.footer}>
                 <a href='https://github.com/rohancs127/Certificate-generator'><div className={HomeCss.githubDiv}><img className={HomeCss.githubLogo} src='github-mark-white.svg'/><div className={HomeCss.githubHeading}>GitHub</div></div></a>
--- src/assets/pages/Input.jsx
+++ src/assets/pages/Input.jsx
@@ -1,8 +1,24 @@
+import InputCss from '../styles/Input.module.css'
+
 const Input = ()=>{
     return(
         <section>
-            <form>
-                <input type="text" placeholder="Name"></input>
+            <form className={InputCss.formDiv}>
+                <h2 className={InputCss.heading}>Enter your details</h2>
+                <div className={InputCss.contentDiv}>
+                    <h3 className={InputCss.tag}>Name</h3>
+                    <input className={InputCss.tagInput} placeholder="Enter your name"></input>
+                </div>
+                <div className={InputCss.contentDiv}>
+                    <h3 className={InputCss.tag}>Branch</h3>
+                    <input className={InputCss.tagInput} placeholder="Enter your branch"></input>
+                </div>
+                <div className={InputCss.contentDiv}>
+                    <h3 className={InputCss.tag}>College</h3>
+                    <input className={InputCss.tagInput} placeholder="Enter your college"></input>
+                </div>
+                <button>Generate</button>
+                <h4 className={InputCss.subtitle}>Click here to generate your certificate</h4>
             </form>
         </section>
     )

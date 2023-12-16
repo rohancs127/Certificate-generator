0fd75beddbb9a17843a35ea21d54803b13212713
Rohan <rohankumar5803@gmail.com>
2023-12-16 21:01:16 +0530

    added route to input.jsx

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,6 +1,11 @@
 import HomeCss from '../styles/Home.module.css'
+import {useNavigate} from 'react-router-dom';
 
 const Home = ()=>{
+    const navigate = useNavigate();
+    const navigateToPage = ()=>{
+        navigate('/generate');
+    }
     return (
         <section className={HomeCss.home}>
             <div className={HomeCss.header}>
@@ -9,7 +14,7 @@ const Home = ()=>{
             <div className={HomeCss.contentBody}>
                 <h3 className={HomeCss.getStarted}>Get Started</h3>
                 <h4 className={HomeCss.generateCertificate}>Generate your certificate here!</h4>
-                <button className={HomeCss.generateButton}>Generate Certificate</button>
+                <button className={HomeCss.generateButton} onClick={navigateToPage}>Generate Certificate</button>
             </div>
             <div className={HomeCss.footer}>
                 <a href='https://github.com/rohancs127/Certificate-generator'><div className={HomeCss.githubDiv}><img className={HomeCss.githubLogo} src='github-mark-white.svg'/><div className={HomeCss.githubHeading}>GitHub</div></div></a>

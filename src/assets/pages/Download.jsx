3e2d90b9c4e3a948a5042e4e6f8913603263bf9d
Rohan <rohankumar5803@gmail.com>
2023-12-17 20:36:20 +0530

    created new download page and style

---
+++ src/assets/pages/Download.jsx
@@ -0,0 +1,12 @@
+import Certificate from "./Cerificate";
+import DownloadCss from '../styles/Download.module.css';
+
+const Download = ()=>{
+    return(
+        <section>
+
+        </section>
+    );
+}
+
+export default Download;

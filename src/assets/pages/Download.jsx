10516b02529ebca294722d5b3e05630fba2ba78e
Rohan <rohankumar5803@gmail.com>
2023-12-19 08:25:56 +0530

    wrapped certificate component inside div

--- src/assets/pages/Download.jsx
+++ src/assets/pages/Download.jsx
@@ -2,9 +2,12 @@ import Certificate from "./Cerificate";
 import DownloadCss from '../styles/Download.module.css';
 
 const Download = ()=>{
+   
     return(
         <section>
-            <Certificate/>
+            <div className={DownloadCss.certificate}>
+                <Certificate/>
+            </div>
         </section>
     );
 }

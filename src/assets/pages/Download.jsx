5c72fec4ae459fb85d1d6efc6c14b1bb2c5d64e4
Rohan <rohankumar5803@gmail.com>
2024-03-21 21:51:20 +0530

    usenav implemented

--- src/assets/pages/Download.jsx
+++ src/assets/pages/Download.jsx
@@ -1,44 +1,47 @@
 import Certificate from "./Certificate";
-import DownloadCss from '../styles/Download.module.css';
-import html2canvas from 'html2canvas';
+import DownloadCss from "../styles/Download.module.css";
+import html2canvas from "html2canvas";
 import certificateData from "../scripts/Certificate";
+import { useNavigate } from "react-router-dom";
 
-const Download = ()=>{
-    const {name, event} = certificateData;
-    const handleDownload = () => {
-        const certificateElement = document.getElementById('certificate-section');
-    
-        html2canvas(certificateElement, {
-          width: certificateElement.offsetWidth,
-          height: certificateElement.offsetHeight, 
-          scrollX: 0,
-          scrollY: -window.scrollY
-        })
-          .then((canvas) => {
-            const image = canvas.toDataURL('image/png');
-            const link = document.createElement('a');
-            link.href = image;
-            link.download = `${name}.png`;
-            link.click();
-          })
-          .catch((error) => {
-            console.error('Error capturing certificate as image:', error);
-          });
-      };
-   
-    return(
-        <section>
-            <div className={DownloadCss.certificate}>
-                <div>
-                <Certificate/>
-                </div>
-                <div className={DownloadCss.buttonDiv}>
-                    <button onClick={handleDownload}>Download</button>
-                    <h4 className={DownloadCss.description}>Click here to Download</h4>
-                </div>
-            </div>
-        </section>
-    );
-}
+const Download = () => {
+  const { name, event } = certificateData;
+  const navigate = useNavigate();
+  const handleDownload = () => {
+    const certificateElement = document.getElementById("certificate-section");
+
+    html2canvas(certificateElement, {
+      width: certificateElement.offsetWidth,
+      height: certificateElement.offsetHeight,
+      scrollX: 0,
+      scrollY: -window.scrollY,
+    })
+      .then((canvas) => {
+        const image = canvas.toDataURL("image/png");
+        const link = document.createElement("a");
+        link.href = image;
+        link.download = `${name}.png`;
+        link.click();
+      })
+      .catch((error) => {
+        console.error("Error capturing certificate as image:", error);
+      });
+    navigate("/home");
+  };
+
+  return (
+    <section>
+      <div className={DownloadCss.certificate}>
+        <div>
+          <Certificate />
+        </div>
+        <div className={DownloadCss.buttonDiv}>
+          <button onClick={handleDownload}>Download</button>
+          <h4 className={DownloadCss.description}>Click here to Download</h4>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Download;

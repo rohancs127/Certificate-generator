e06322989621af80cc4aa6c01157fbe4b97f1316
Rohan <rohankumar5803@gmail.com>
2024-08-04 22:27:18 +0530

    add style to download complete div

--- src/Components/DownloadComplete.jsx
+++ src/Components/DownloadComplete.jsx
@@ -6,9 +6,11 @@ import { Link } from "react-router-dom";
 function DownloadComplete() {
   return (
     <Link to="/home">
-      <div className="d-c-section">
-        <h1 className="d-c-heading">Download Complete</h1>
-        <CircleCheck size={50} strokeWidth={2} />
+      <div className="d-c-maindiv">
+        <div className="d-c-section">
+          <h1 className="d-c-heading">Download Complete</h1>
+          <CircleCheck size={50} strokeWidth={2} />
+        </div>
       </div>
     </Link>
   );
